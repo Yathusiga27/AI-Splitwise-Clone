80d38ab73d5d2df52a74caafb2959df6aaee4aae
Yathusiga27 <yathu2708@gmail.com>
2025-12-20 22:22:50 +0530

    create contacts page, create groups

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,34 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
-import { ConvexClientProvider } from "../components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import Header from "@/components/header";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splitr",
-  description: "The Smartest Way to Split Expenses with Friends",
+  description: "The smartest way to split expenses with friends",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logos/logo-s.png" size="any" />
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
-      <body className={`${inter.className} `}>
-        <ClerkProvider>
+      <body className={`${inter.className}`}>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
           <ConvexClientProvider>
             <Header />
-            <main className="min-h-screen">{children}</main>
+            <main className="min-h-screen">
+              <Toaster richColors />
+
+              {children}
+            </main>
           </ConvexClientProvider>
         </ClerkProvider>
       </body>
